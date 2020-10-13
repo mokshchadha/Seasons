4eca9f6c32bc88c0a2d4af1dec02abcb5ebd6a69
moksh <moksh@source.one>
2020-10-14 02:46:41 +0530

    adding the loading spinner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null };
@@ -15,7 +16,7 @@ class App extends React.Component {
 
   render() {
     if (this.state.lat) return <SeasonDisplay lat={this.state.lat} />;
-    else return <div>Loading</div>;
+    else return <Spinner />;
   }
 }
 
